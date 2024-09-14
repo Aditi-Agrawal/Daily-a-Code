09216f74c6b22229526b30bdcbfac63e02ac0dd4
Aditi-Agrawal <54217725+Aditi-Agrawal@users.noreply.github.com>
2024-09-14 23:19:08 +0530

    Time: 2 ms (83.11%) | Memory: 44.2 MB (93.21%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<Integer> list = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        helper(0, target,candidates, new ArrayList<>(),ans);
+        helper(0, target, candidates, list, ans);
         return ans;
     }
 
-    private void helper(int ind, int target, int[] arr, List<Integer> list, List<List<Integer>> ans ){
-        if(ind==arr.length){
-            if(target==0){
-                ans.add(new ArrayList<>(list));
-            }
+    public void helper(int ind, int target, int[] candidates, List<Integer> list, List<List<Integer>> ans){
+        if(target==0){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        if(arr[ind]<=target){
-            //pick
-            list.add(arr[ind]);
-            helper(ind,target-arr[ind],arr,list,ans);
+        if(ind<candidates.length){
+        //pick
+        if(target>=candidates[ind]){
+            list.add(candidates[ind]);
+            helper(ind,target-candidates[ind], candidates, list, ans);
             list.remove(list.size()-1);
-            //not pick
         }
-        helper(ind+1,target,arr,list,ans);
+        ///not pick
+        helper(ind+1, target, candidates, list, ans);
+        }
     }
 }
