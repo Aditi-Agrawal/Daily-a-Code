ff5a5e0c40c00643650509d75dd56b87c4805004
Aditi-Agrawal <54217725+Aditi-Agrawal@users.noreply.github.com>
2024-09-15 00:18:30 +0530

    Time: 3 ms (77.92%) | Memory: 43.2 MB (58.61%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,29 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<Integer> ds = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
-        f(0,target,candidates,ds,ans);
+        helper(0, target, candidates, list, ans);
         return ans;
     }
-
-    private void f(int ind, int target ,int[] arr, List<Integer> ds, List<List<Integer>> ans){
+    public void helper(int ind, int target, int[] arr, List<Integer> list, List<List<Integer>> ans){
         if(target==0){
-            ans.add(new ArrayList<>(ds));
+            ans.add(new ArrayList<>(list));
             return;
         }
-
-        for(int i=ind;i<arr.length;i++){
-
+        for(int i = ind;i<arr.length;i++){
             if(i>ind && arr[i]==arr[i-1]) continue;
-
             if(arr[i]>target) break;
 
-            if(arr[i]<=target){
-                ds.add(arr[i]);
-                f(i+1,target-arr[i],arr,ds,ans);
-                ds.remove(ds.size()-1);
-            }
+            list.add(arr[i]);
+            helper(i+1,target-arr[i],arr,list,ans);
+            list.remove(list.size()-1);
         }
     }
 }
