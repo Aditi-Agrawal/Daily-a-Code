10f9a2c3f6e70c5a02649d10c66b3084c329240c
Aditi-Agrawal <54217725+Aditi-Agrawal@users.noreply.github.com>
2024-09-16 00:45:45 +0530

    Time: 8 ms (55.17%) | Memory: 57 MB (43.17%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,33 +1,30 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
         List<String> list = new ArrayList<>();
-        func(0,s,list,ans);
+        List<List<String>> ans = new ArrayList<>();
+        func(0, s, list, ans);
         return ans;
     }
 
-    private void func(int index, String s, List<String> list, List<List<String>> ans){
+    void func(int index, String s, List<String> list, List<List<String>> ans){
         if(index == s.length()){
-            ans.add(new ArrayList<>(list));
+            ans.add(new ArrayList(list));
             return;
         }
-
-        for(int i = index; i<s.length();i++){
-            if(isPalindrome(s,index,i)){
+        for(int i=index;i<s.length();i++){
+            if(isPalindrome(s, index,i)){
                 list.add(s.substring(index,i+1));
-                func(i+1, s,list,ans);
+                func(i+1,s,list,ans);
                 list.remove(list.size()-1);
             }
         }
-        return;
     }
 
-    private boolean isPalindrome(String s, int start, int end){
-        while(start<=end){
+    private boolean isPalindrome(String s,int start, int end){
+        while(start <= end){
             if(s.charAt(start++) != s.charAt(end--))
                 return false;
         }
         return true;
     }
-
 }
